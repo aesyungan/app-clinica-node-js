f7c5ebb3121aa88e1b6b3c89ce77760b61c7631c
alex yungan <ayungan@localhost.localdomain>
2018-11-04 13:59:43 -0500

    token

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -6,14 +6,15 @@ const HttpStatus = require('../config/http.status.config');
 const sms = require('../config/messages.config');
 let ResponseData = require('../utils/ResponseData');
 isAuth = (req, res, next) => {
+    console.log(req.headers.authorization);
     if (!req.headers.authorization) {
         return res.status(HttpStatus.Forbidden).send(new ResponseData(true, sms.Access, sms.detailsForbidden, null));
     }
     try {
         let token = req.headers.authorization.split(" ")[1];//combierte la cavecera en array y el token es el 1 
         let payload = jwt.decode(token, authConfig.SECRET_TOKEN);
-        console.log("token");
-        console.log(payload);
+        // console.log("token");
+        // console.log(payload);
         if (payload.exp < moment.unix()) {
             return res.status(HttpStatus.Unauthorized).send(new ResponseData(true, sms.Access, sms.detailsTokenExpired, null));
         }
